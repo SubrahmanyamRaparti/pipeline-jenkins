3d1dd36304fd3c086c769645649084c7d171d722
SubrahmanyamRaparti <rv.subrahmanyam1@gmail.com>
2022-03-20 19:55:28 +0530

    Moved to other system

--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -2,6 +2,6 @@
 
 echo '+ Testing the Hello World application +'
 
-WORKSPACE=/home/ubuntu/jenkins/jenkins_home/workspace/pipeline-work2
+WORKSPACE=/home/subrahmanyam/jenkins/jenkins_home/workspace/pipeline-work2
 
 docker container run --rm -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:ibmjava-alpine "$@"
