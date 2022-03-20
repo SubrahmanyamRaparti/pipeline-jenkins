afba70ef1c90d6d3589e3fbc1a4170fd2891435d
rv.subrahmanyam1 <rv.subrahmanyam1@gamil.com>
2022-03-20 05:05:18 +0000

    Updated relative paths

--- jenkins/build/build.sh
+++ jenkins/build/build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 echo '+ Copying Jar file "/home/ubuntu/jenkins/pipeline/jenkins/build/" location +'
-cp -f /home/ubuntu/jenkins/pipeline/java-app/target/*jar /home/ubuntu/jenkins/pipeline/jenkins/build/
+cp -f ./java-app/target/*jar ./jenkins/build/
 echo '+ Copy complete +'
 
 
 echo '+ Building Docker Image +'
 echo ' '
-cd /home/ubuntu/jenkins/pipeline/jenkins/build &&
+cd ./jenkins/build &&
     docker-compose build --no-cache
 echo ' '
 echo '+ Image build complete +'
--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -4,17 +4,17 @@ echo subrahmanyamr/java_app > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $DOCKER_PASS_KEY >> /tmp/.auth
 
-IPV4=ec2-13-233-131-230.ap-south-1.compute.amazonaws.com
+IPV4=ec2-13-233-9-11.ap-south-1.compute.amazonaws.com
 
 scp -i "/home/ubuntu/Linux.pem" /tmp/.auth ubuntu@$IPV4:/tmp/.auth
 
 echo '1. Copied /tmp/.auth file'
 
-scp -i "/home/ubuntu/Linux.pem" /home/ubuntu/jenkins/pipeline/jenkins/deploy/publish.sh ubuntu@$IPV4:/tmp/publish.sh
+scp -i "/home/ubuntu/Linux.pem" ./jenkins/deploy/publish.sh ubuntu@$IPV4:/tmp/publish.sh
 
 echo '2. Copied publish.sh file'
 
-scp -i "/home/ubuntu/Linux.pem" -r /home/ubuntu/jenkins/pipeline/jenkins/deploy/maven/ ubuntu@$IPV4:
+scp -i "/home/ubuntu/Linux.pem" -r ./jenkins/deploy/maven/ ubuntu@$IPV4:
 
 echo '3. Copied maven/docker-compose.yml file'
 
