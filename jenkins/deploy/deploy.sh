f79c0ef4163be9b2463fc0769edbeee94795abd6
SubrahmanyamRaparti <rv.subrahmanyam1@gmail.com>
2022-03-20 20:25:43 +0530

    update host id

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -4,20 +4,15 @@ echo subrahmanyamr/java_app > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $DOCKER_PASS_KEY >> /tmp/.auth
 
-IPV4=ec2-3-108-234-12.ap-south-1.compute.amazonaws.com
-
-scp -i "/home/ubuntu/Linux.pem" /tmp/.auth ubuntu@$IPV4:/tmp/.auth
-
+scp -i "/home/ubuntu/Linux.pem" /tmp/.auth ubuntu@ec2-3-108-234-12.ap-south-1.compute.amazonaws.com:/tmp/.auth && \
 echo '1. Copied /tmp/.auth file'
 
-scp -i "/home/ubuntu/Linux.pem" ./jenkins/deploy/publish.sh ubuntu@$IPV4:/tmp/publish.sh
-
+scp -i "/home/ubuntu/Linux.pem" ./jenkins/deploy/publish.sh ubuntu@ec2-3-108-234-12.ap-south-1.compute.amazonaws.com:/tmp/publish.sh && \
 echo '2. Copied publish.sh file'
 
-scp -i "/home/ubuntu/Linux.pem" -r ./jenkins/deploy/maven/ ubuntu@$IPV4:
-
+scp -i "/home/ubuntu/Linux.pem" -r ./jenkins/deploy/maven/ ubuntu@ec2-3-108-234-12.ap-south-1.compute.amazonaws.com: && \
 echo '3. Copied maven/docker-compose.yml file'
 
-ssh -i "/home/ubuntu/Linux.pem" ubuntu@IPV4 "/tmp/publish.sh"
+ssh -i "/home/ubuntu/Linux.pem" ubuntu@ec2-3-108-234-12.ap-south-1.compute.amazonaws.com "/tmp/publish.sh"
 
 rm -fr /tmp/.auth
