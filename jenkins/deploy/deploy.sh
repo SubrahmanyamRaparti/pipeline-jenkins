8aea17d0d125e380c30ec7851fa96b285218788a
SubrahmanyamRaparti <rv.subrahmanyam1@gmail.com>
2022-03-20 21:08:10 +0530

    removed sudo in deplot file

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -4,15 +4,15 @@ echo subrahmanyamr/java_app > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $DOCKER_PASS_KEY >> /tmp/.auth
 
-sudo scp -i "/home/ubuntu/Linux.pem" /tmp/.auth ubuntu@ec2-3-108-234-12.ap-south-1.compute.amazonaws.com:/tmp/.auth && \
+scp -i "/home/ubuntu/Linux.pem" /tmp/.auth ubuntu@ec2-3-108-234-12.ap-south-1.compute.amazonaws.com:/tmp/.auth && \
 echo '1. Copied /tmp/.auth file'
 
-sudo scp -i "/home/ubuntu/Linux.pem" ./jenkins/deploy/publish.sh ubuntu@ec2-3-108-234-12.ap-south-1.compute.amazonaws.com:/tmp/publish.sh && \
+scp -i "/home/ubuntu/Linux.pem" ./jenkins/deploy/publish.sh ubuntu@ec2-3-108-234-12.ap-south-1.compute.amazonaws.com:/tmp/publish.sh && \
 echo '2. Copied publish.sh file'
 
-sudo scp -i "/home/ubuntu/Linux.pem" -r ./jenkins/deploy/maven/ ubuntu@ec2-3-108-234-12.ap-south-1.compute.amazonaws.com: && \
+scp -i "/home/ubuntu/Linux.pem" -r ./jenkins/deploy/maven/ ubuntu@ec2-3-108-234-12.ap-south-1.compute.amazonaws.com: && \
 echo '3. Copied maven/docker-compose.yml file'
 
-sudo ssh -i "/home/ubuntu/Linux.pem" ubuntu@ec2-3-108-234-12.ap-south-1.compute.amazonaws.com "/tmp/publish.sh"
+ssh -i "/home/ubuntu/Linux.pem" ubuntu@ec2-3-108-234-12.ap-south-1.compute.amazonaws.com "/tmp/publish.sh"
 
 rm -fr /tmp/.auth
